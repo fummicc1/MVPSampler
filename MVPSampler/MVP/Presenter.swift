811e6c3822fa663b41bca61d809c4e0c525b4f7c
fummicc1 <fumiya989@gmail.com>
2019-04-16 20:33:49 +0900

    fix

--- MVPSampler/MVP/Presenter.swift
+++ MVPSampler/MVP/Presenter.swift
@@ -1,15 +1,21 @@
 import Foundation
 
+protocol Output {
+    var qiitaList: [Qiita]? { get }
+}
+
 protocol PresentationLogic {
     func startLoadingQiita()
     func successFetchingQiita()
     func failedFetchingQiita()
 }
 
-class Presenter: PresentationLogic {
+class Presenter: PresentationLogic, Output {
     
     weak var viewController: ViewLogic?
     var model: Model?
+    
+    var qiitaList: [Qiita]?
     
     init(viewController: ViewController, model: Model = Model()) {
         self.viewController = viewController
@@ -18,11 +24,17 @@ class Presenter: PresentationLogic {
     
     func startLoadingQiita() {
         guard let url = QiitaAPI.url else { return }
-        model?.load(url: url, completion: { [weak self] (qiita) in
-            self?.viewController?.displayQiita(qiita: qiita)
+        model?.load(url: url, completion: { [weak self] (qiitaList) in
+            self?.qiitaList = qiitaList
+            self?.presentRandomQiita()
         }, failure: { [weak self] in
             self?.viewController?.showError()
         })
+    }
+    
+    func presentRandomQiita() {
+        guard let qiita = qiitaList?.randomElement() else { return }
+        viewController?.displayQiita(qiita: qiita)
     }
     
     func successFetchingQiita() {
